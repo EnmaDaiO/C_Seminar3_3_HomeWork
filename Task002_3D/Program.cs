5e0026ca470e1e0e20575d9b578261f62f5d74fc
Eldar Gyulmagomedov <steal.vip@gmail.com>
2023-02-24 18:09:45 +0300

    Решили вторую задачу по нахождению расстояния между двумя точками в 3D

--- Task002_3D/Program.cs
+++ Task002_3D/Program.cs
@@ -5,3 +5,27 @@
 
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
+// if - else if - else
+
+// корень((х2-х1)^2 + (у2-у1)^2 + (z2 - z1)^2) = Формула нахождения расстояния между двумя точками в 3D
+
+//A(x1;    y1)
+Console.Write("Введите координату точки A по оси Х: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату точки  A по оси Y: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату точки A по оси Z: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+// B(x2;    y2)
+Console.Write("Введите координату точки B по оси Х: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату точки B по оси Y: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату точки B по оси Z: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+// AB = √(x2 - x1)^2 + (y2 - y1)^2 + (z2 - z1)^2
+// Math.Sqrt(25) => 5
+// Math.Pow(число, степень) => Math.Pow(4, 2) => 4 возводите во 2 степень = 16
+var result = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1,2) + Math.Pow(z2 - z1,2));
+Console.WriteLine(Math.Round(result, 2)); 
